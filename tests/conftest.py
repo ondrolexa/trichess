e60d5a75d6b0adeacb9833f5595b095f66682429
Ondrej Lexa <lexa.ondrej@gmail.com>
2025-03-26 22:04:33 +0100

    some tests added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,23 @@
 import pytest
 
-from trichess import Board
+from trichess import Board, Player
 
 
 @pytest.fixture
 def board():
     return Board()
+
+
+@pytest.fixture
+def player0():
+    return Player(0)
+
+
+@pytest.fixture
+def player1():
+    return Player(1)
+
+
+@pytest.fixture
+def player2():
+    return Player(2)
--- tests/test_trichess.py
+++ tests/test_trichess.py
@@ -1,6 +1,28 @@
 import pytest
 
+from trichess import Move, Pos
 
-def test_min(dem):
-    assert dem.min == pytest.approx(508.242681917153)
 
+class Test_Board:
+    def test_pos_gid(self, board):
+        assert board[Pos(-4, 2)] is board.gid["32"]
+
+
+class Test_Player_Steps:
+    def test_player0_fl(self, player0):
+        assert player0.pos_from_move(Pos(0, 0), Move("FL")) == Pos(0, -1)
+
+    def test_player0_fr(self, player0):
+        assert player0.pos_from_move(Pos(0, 0), Move("FR")) == Pos(1, -1)
+
+    def test_player1_fl(self, player1):
+        assert player1.pos_from_move(Pos(0, 0), Move("FL")) == Pos(-1, 1)
+
+    def test_player1_fr(self, player1):
+        assert player1.pos_from_move(Pos(0, 0), Move("FR")) == Pos(-1, 0)
+
+    def test_player2_fl(self, player2):
+        assert player2.pos_from_move(Pos(0, 0), Move("FL")) == Pos(1, 0)
+
+    def test_player2_fr(self, player2):
+        assert player2.pos_from_move(Pos(0, 0), Move("FR")) == Pos(0, 1)
