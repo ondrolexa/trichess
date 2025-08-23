5445e58cb6c817233443cc7af6dadbf2e3f93afb
Ondrej Lexa <lexa.ondrej@gmail.com>
2025-08-24 00:16:18 +0200

    email unique constraint removed

--- webapp/models.py
+++ webapp/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(500))
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120))
     score = db.Column(db.Float)
     theme = db.Column(db.Integer)
 
